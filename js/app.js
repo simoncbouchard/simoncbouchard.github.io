30428047c2cd261508ffd48bc66fceda085ce50c
Simon Côté-Bouchard <simoncote-bouchard@Simons-MacBook-Pro.local>
2017-04-21 15:00:23 -0400

    projet biero final

--- js/app.js
+++ js/app.js
@@ -1,110 +1,228 @@
-
 //IIFE
 (function(){
     
-    window.addEventListener("load", function(){                                                         // au chargement de la page
+    window.addEventListener("load", function(){                                                             // au chargement de la page
         
-        /*--  RÉCUPÈRE LE JSON DU WEBSERVICE  ------------------------------------------------*/
         
-        if(navigator.onLine){                                                                           // si en ligne
+        if(document.querySelector(".bieres")){                                                              // si la section 'bieres' existe
             
-            /*       
-            // source : https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
-            
-            var invocation = new XMLHttpRequest();
-            var url = 'http://www.simoncbouchard.ca/webservice/biero/biere';
-   
-            function callOtherDomain() {
-                if(invocation) {    
-                  invocation.open('GET', url, true);
-                  invocation.onreadystatechange = handler;
-                  invocation.send(); 
-                }
-            }
-            
-            var biere = callOtherDomain();
-            */
-            
-            
-            // source : http://stackoverflow.com/questions/12460378/how-to-get-json-from-url-in-javascript
-            
-            //var url = 'http://www.simoncbouchard.ca/webservice/biero/biere';    
-            var url = ' http://www.jonathanmartel.info/cors/?url=http://www.simoncbouchard.ca/webservice/biero/biere'; 
-            
-            fetch(url)
-            .then(res => res.json())
-            .then((biere) => {
-                console.log(biere);
+            /*--  SI EN LIGNE  -----------------------------------------------------------------------*/
+            if(navigator.onLine){
                 
-                // source : http://stackoverflow.com/questions/2010892/storing-objects-in-html5-localstorage
-                localStorage.setItem('jsonDesBieresEnString', JSON.stringify(biere));                   // sauvegarde le JSON obtenu en localStorage
+                var divEnLigne = document.createElement("div");                                             // div enLigne
+                divEnLigne.className = 'classeDivEnLigne';
+                document.querySelector(".entete").appendChild(divEnLigne);
                 
+                var divRond = document.createElement("div");                                                // div rond vert 
+                divRond.className = 'classeDivRondVert';
+                divEnLigne.appendChild(divRond);
                 
-                for(i=0; i<biere.length; i++){
-                    /*--  DOM  -------------------------------------------------------------------*/
+                var parVousEteConnecte = document.createElement("p");                                       // chaîne "connecté"
+                parVousEteConnecte.className = 'classeParVousEteConnecte';
+                divEnLigne.appendChild(parVousEteConnecte);
+                var texteVousEteConnecte = document.createTextNode("Vous pouvez consulter cette page offline.");
+                parVousEteConnecte.appendChild(texteVousEteConnecte);
+                
+                // source : http://stackoverflow.com/questions/12460378/how-to-get-json-from-url-in-javascript
+                
+                /*--  RÉCUPÈRE LE JSON DU WEBSERVICE  ------------------------------------------------*/
+                //var url = '../../webservice/biero/biere';    
+                var url = 'https://www.simoncbouchard.ca/webservice/biero/biere';    
+                
+                fetch(url)
+                .then(res => res.json())
+                .then((biere) => {
+                    console.log(biere);
                     
-                    var nouvelleBiere = document.createElement("a"); 
-                    nouvelleBiere.href = "#";
+                    // source : http://stackoverflow.com/questions/2010892/storing-objects-in-html5-localstorage
+                    localStorage.setItem('jsonDesBieresEnString', JSON.stringify(biere));                   // sauvegarde le JSON obtenu en localStorage
+                    
+                    for(i=0; i<biere.length; i++){
+                        
+                        /*--  DOM  -------------------------------------------------------------------*/
+                        
+                        var nouvelleBiere = document.createElement("a");                                    // lien
+                        nouvelleBiere.href = "./biere.html#" + biere[i]["id_biere"];
+                        nouvelleBiere.className = 'chaqueBiere';
+                        document.querySelector(".bieres").appendChild(nouvelleBiere);
+                        
+                        var imageBiere = document.createElement("IMG");                                     // image
+                        imageBiere.setAttribute("src", biere[i]["image"]);
+                        imageBiere.setAttribute("width", "304");
+                        imageBiere.setAttribute("height", "304");
+                        imageBiere.setAttribute("alt", biere[i]["nom"]);
+                        nouvelleBiere.appendChild(imageBiere);
+                        
+                        var parNom = document.createElement("p");                                           // nom de la bière
+                        parNom.className = 'nomDeLaBiere';
+                        nouvelleBiere.appendChild(parNom);
+                        var nom = document.createTextNode(biere[i]["nom"]);
+                        parNom.appendChild(nom);
+                        
+                        var parBrasserie = document.createElement("p");                                     // nom de la brasserie
+                        parBrasserie.className = 'brasserieDeLaBiere';
+                        nouvelleBiere.appendChild(parBrasserie);
+                        var brasserie = document.createTextNode(biere[i]["brasserie"]);
+                        parBrasserie.appendChild(brasserie);
+                        
+                    }
+                })
+                .catch(err => console.error(err));
+            }
+            else{
+                
+                var divHorsLigne = document.createElement("div");                                           // div horsLigne
+                divHorsLigne.className = 'classeDivHorsLigne';
+                document.querySelector(".entete").appendChild(divHorsLigne);
+                
+                var divRondRouge = document.createElement("div");                                           // div rond rouge
+                divRondRouge.className = 'classeDivRondRouge';
+                divHorsLigne.appendChild(divRondRouge);
+                
+                var parVousNetePasConnecte = document.createElement("p");                                   // chaîne "offline"
+                parVousNetePasConnecte.className = 'classeParVousNetePasConnecte';
+                divHorsLigne.appendChild(parVousNetePasConnecte);
+                var texteVousNetePasConnecte = document.createTextNode("Cette page s'ouvre offline.");
+                parVousNetePasConnecte.appendChild(texteVousNetePasConnecte);
+                
+                /*--  RÉCUPÈRE LE JSON PLACÉ EN LOCALHOST  -------------------------------------------*/
+                var recupereLeJsonDesBieres = localStorage.getItem('jsonDesBieresEnString');
+                var biereOffline = JSON.parse(recupereLeJsonDesBieres);                                     // parse le JSON
+                //console.log(biereOffline);
+                
+                for(i=0; i<biereOffline.length; i++){
+                    
+                    /*--  DOM  -----------------------------------------------------------------------*/
+                    
+                    var nouvelleBiere = document.createElement("a");                                        // lien
+                    nouvelleBiere.href = "./biere.html#" + biereOffline[i]["id_biere"];
                     nouvelleBiere.className = 'chaqueBiere';
                     document.querySelector(".bieres").appendChild(nouvelleBiere);
                     
-                    var imageBiere = document.createElement("IMG");
-                    imageBiere.setAttribute("src", biere[i]["image"]);
+                    var imageBiere = document.createElement("IMG");                                         // image
+                    imageBiere.setAttribute("src", biereOffline[i]["image"]);
                     imageBiere.setAttribute("width", "304");
                     imageBiere.setAttribute("height", "304");
-                    imageBiere.setAttribute("alt", biere[i]["nom"]);
+                    imageBiere.setAttribute("alt", biereOffline[i]["nom"]);
                     nouvelleBiere.appendChild(imageBiere);
                     
-                    var parNom = document.createElement("p"); 
+                    var parNom = document.createElement("p");                                               // nom de la bière 
                     parNom.className = 'nomDeLaBiere';
                     nouvelleBiere.appendChild(parNom);
-                    var nom = document.createTextNode(biere[i]["nom"]);
+                    var nom = document.createTextNode(biereOffline[i]["nom"]);
                     parNom.appendChild(nom);
                     
-                    var parBrasserie = document.createElement("p");
+                    var parBrasserie = document.createElement("p");                                         // nom de la brasserie
                     parBrasserie.className = 'brasserieDeLaBiere';
                     nouvelleBiere.appendChild(parBrasserie);
-                    var brasserie = document.createTextNode(biere[i]["brasserie"]);
+                    var brasserie = document.createTextNode(biereOffline[i]["brasserie"]);
                     parBrasserie.appendChild(brasserie);
-                    
-                }
-            })
-            .catch(err => console.error(err));
+                        
+                }  
+            }
         }
-        else{
+        
+        if(document.querySelector(".biere")){                                                               // si la section 'biere' existe
+            
+            /*--  SI EN LIGNE  -----------------------------------------------------------------------*/
+            if(navigator.onLine){                                                                           // si en ligne vert 
+                
+                var divEnLigne = document.createElement("div");                                             // div enLigne
+                divEnLigne.className = 'classeDivEnLigne';
+                document.querySelector(".entete").appendChild(divEnLigne);
+                
+                var divRond = document.createElement("div");                                                // div rond
+                divRond.className = 'classeDivRondVert';
+                divEnLigne.appendChild(divRond);
+                
+                var parVousEteConnecte = document.createElement("p");                                       // chaîne "connecté"
+                parVousEteConnecte.className = 'classeParVousEteConnecte';
+                divEnLigne.appendChild(parVousEteConnecte);
+                var texteVousEteConnecte = document.createTextNode("Vous pouvez consulter cette page offline.");
+                parVousEteConnecte.appendChild(texteVousEteConnecte);
+            }
+            /*--  SI HORS LIGNE  ---------------------------------------------------------------------*/
+            else{
+                var divHorsLigne = document.createElement("div");                                           // div horsLigne
+                divHorsLigne.className = 'classeDivHorsLigne';
+                document.querySelector(".entete").appendChild(divHorsLigne);
+                
+                var divRondRouge = document.createElement("div");                                           // div rond rouge
+                divRondRouge.className = 'classeDivRondRouge';
+                divHorsLigne.appendChild(divRondRouge);
+                
+                var parVousNetePasConnecte = document.createElement("p");                                   // chaîne "offline"
+                parVousNetePasConnecte.className = 'classeParVousEteConnecte';
+                divHorsLigne.appendChild(parVousNetePasConnecte);
+                var texteVousNetePasConnecte = document.createTextNode("Cette page s'ouvre offline.");
+                parVousNetePasConnecte.appendChild(texteVousNetePasConnecte);
+            }
+            
+            
+            /*--  RÉCUPÈRE L'ID DE LA BIERE SPECIFIÉ EN PARAMÈTRE  ------------------------------------*/
+            var urlRecupere = window.location.href;
+            var paramId = urlRecupere.split('#');
+            var idDeLaBiere = paramId[1]-18;
+            //console.log(idDeLaBiere);
+            
+            /*--  RÉCUPÈRE LE JSON PLACÉ EN LOCALHOST  ------------------------------------------------*/
             var recupereLeJsonDesBieres = localStorage.getItem('jsonDesBieresEnString');
-            console.log(JSON.parse(recupereLeJsonDesBieres));
+            var uneBiereOffline = JSON.parse(recupereLeJsonDesBieres);                                      // parse le JSON
+            //console.log(JSON.parse(uneBiereOffline));                                               
             
-            var biereOffline = JSON.parse(recupereLeJsonDesBieres);
+            /*--  DOM  --------------------------------------------------------------------------------*/
             
-            for(i=0; i<biereOffline.length; i++){
-                /*--  DOM  -------------------------------------------------------------------*/
-                
-                var nouvelleBiere = document.createElement("a"); 
-                nouvelleBiere.href = "#";
-                nouvelleBiere.className = 'chaqueBiere';
-                document.querySelector(".bieres").appendChild(nouvelleBiere);
-                
-                var imageBiere = document.createElement("IMG");
-                imageBiere.setAttribute("src", biereOffline[i]["image"]);
-                imageBiere.setAttribute("width", "304");
-                imageBiere.setAttribute("height", "304");
-                imageBiere.setAttribute("alt", biereOffline[i]["nom"]);
-                nouvelleBiere.appendChild(imageBiere);
-                
-                var parNom = document.createElement("p"); 
-                parNom.className = 'nomDeLaBiere';
-                nouvelleBiere.appendChild(parNom);
-                var nom = document.createTextNode(biereOffline[i]["nom"]);
-                parNom.appendChild(nom);
-                
-                var parBrasserie = document.createElement("p");
-                parBrasserie.className = 'brasserieDeLaBiere';
-                nouvelleBiere.appendChild(parBrasserie);
-                var brasserie = document.createTextNode(biereOffline[i]["brasserie"]);
-                parBrasserie.appendChild(brasserie);
-                    
-            }  
+            var uneBierePageImage = document.createElement("div");                                          // div image
+            uneBierePageImage.className = 'uneBiereImage';
+            document.querySelector(".biere").appendChild(uneBierePageImage);
+            
+            var imageBierePage = document.createElement("IMG");                                             // image
+            imageBierePage.setAttribute("src", uneBiereOffline[idDeLaBiere]["image"]);
+            imageBierePage.setAttribute("width", "304");
+            imageBierePage.setAttribute("height", "304");
+            imageBierePage.setAttribute("alt", uneBiereOffline[idDeLaBiere]["nom"]);
+            uneBierePageImage.appendChild(imageBierePage);
+            
+            var uneBierePage = document.createElement("div");                                               // div texte
+            uneBierePage.className = 'uneBiereTexte';
+            document.querySelector(".biere").appendChild(uneBierePage);
+            
+            var parNomPage = document.createElement("p");                                                   // nom de la bière 
+            parNomPage.className = 'nomDeLaBierePage';
+            uneBierePage.appendChild(parNomPage);
+            var nomPage = document.createTextNode(uneBiereOffline[idDeLaBiere]["nom"]);
+            parNomPage.appendChild(nomPage);
+            
+            var parBrasseriePage = document.createElement("p");                                             // nom de la brasserie
+            parBrasseriePage.className = 'brasserieDeLaBierePage';
+            uneBierePage.appendChild(parBrasseriePage);
+            var brasseriePage = document.createTextNode(uneBiereOffline[idDeLaBiere]["brasserie"]);
+            parBrasseriePage.appendChild(brasseriePage);
+            
+            var parMoyennePage = document.createElement("p");                                               // note moyenne
+            parMoyennePage.className = 'moyenneDeLaBierePage';
+            uneBierePage.appendChild(parMoyennePage);
+            var moyennePage = document.createTextNode(Math.floor(uneBiereOffline[idDeLaBiere]["moyenne"]));
+            parMoyennePage.appendChild(moyennePage);
+            
+            var spanSurDix = document.createElement("span");                                               // sur 10
+            spanSurDix.className = 'surDixDeLaBierePage';
+            uneBierePage.appendChild(spanSurDix);
+            var surDix = document.createTextNode(" / 10");
+            spanSurDix.appendChild(surDix);
+            
+            var parNombreNotePage = document.createElement("p");                                            // nombre de notes
+            parNombreNotePage.className = 'nombreNoteDeLaBierePage';
+            uneBierePage.appendChild(parNombreNotePage);
+            var nombreNotePage = document.createTextNode("Nombre de votes : " + uneBiereOffline[idDeLaBiere]["nombre"]);
+            parNombreNotePage.appendChild(nombreNotePage);
+        
+            var parDescriptionPage = document.createElement("p");                                           // description
+            parDescriptionPage.className = 'descriptionDeLaBierePage';
+            uneBierePage.appendChild(parDescriptionPage);
+            var descriptionPage = document.createTextNode(uneBiereOffline[idDeLaBiere]["description"]);
+            parDescriptionPage.appendChild(descriptionPage);
+            
         }
      });
 })();   
--- sw.js
+++ sw.js
@@ -11,26 +11,27 @@ var urlsToCache = [
 
 /*--  INSTALLATION DES CACHES  ------------------------------------------------------*/
 
-self.addEventListener('install', function(event) {                                  // à l'évènment 'install' -> première navigation sur le site
-    event.waitUntil(                                                                // sur l'évènement, attendre jusqu'à (extend la période d'écoute de l'évènement pour permettre les opérations asynchrones)
-        caches.open(CACHE_NAME)                                                     // ouvre le cache au nom de la variable placé en paramètre
-            .then(function(cache) {                                                 // callback
+self.addEventListener('install', function(event) {                                      // à l'évènment 'install' -> première navigation sur le site
+    event.waitUntil(                                                                    // sur l'évènement, attendre jusqu'à (extend la période d'écoute de l'évènement pour permettre les opérations asynchrones)
+        caches.open(CACHE_NAME)                                                         // ouvre le cache au nom de la variable placé en paramètre
+            .then(function(cache) {                                                     // callback
                 console.log('opened cache');
-                return cache.addAll(urlsToCache);                                   // ajoute en cache au nom de la variable ouverte le contenu du tableau d'URL à placer en cache
+                return cache.addAll(urlsToCache);                                       // ajoute en cache au nom de la variable ouverte le contenu du tableau d'URL à placer en cache
             })
     );
 });
 
+
 /*--  RETOUR DE REQUÊTES  -----------------------------------------------------------*/
 
-self.addEventListener('fetch', function(event) {                                    // à l'évènement 'fetch' que reçoit le 'service worker' lors des navigations suivantes
+self.addEventListener('fetch', function(event) {                                        // à l'évènement 'fetch' que reçoit le 'service worker' lors des navigations suivantes
     console.log('fetch cache');
     console.log(event.request.url);
-    event.respondWith(                                                              // sur l'évènement, répond avec 
-    caches.match(event.request)                                                     // retourne une promesse avec la reponse associé au match de l'objet Cache (sinon undefined)
-      .then(function(response) {                                                    // fait avec la réponse : 
-        if (response) {                                                             // s'il y a une réponse (pas undefined)
-          return response;                                                          // retourne la réponse
+    event.respondWith(                                                                  // sur l'évènement, répond avec 
+    caches.match(event.request)                                                         // retourne une promesse avec la reponse associé au match de l'objet Cache (sinon undefined)
+      .then(function(response) {                                                        // fait avec la réponse : 
+        if (response) {                                                                 // s'il y a une réponse (pas undefined)
+          return response;                                                              // retourne la réponse
         }
 
         // IMPORTANT: Clone the request. A request is a stream and
@@ -39,10 +40,10 @@ self.addEventListener('fetch', function(event) {                                
         // to clone the response.
         var fetchRequest = event.request.clone();
 
-        return fetch(fetchRequest).then(                                            // call back sur le fetch
+        return fetch(fetchRequest).then(                                                // call back sur le fetch
           function(response) {
-            if(!response || response.status !== 200 || response.type !== 'basic') { // vérifie si la réponse obtenue est valide
-              return response;                                                      // retourne la réponse
+            if(!response || response.status !== 200 || response.type !== 'basic') {     // vérifie si la réponse obtenue est valide
+              return response;                                                          // retourne la réponse
             }
 
             // IMPORTANT: Clone the response. A response is a stream
@@ -51,12 +52,12 @@ self.addEventListener('fetch', function(event) {                                
             // to clone it so we have two streams.
             var responseToCache = response.clone();
 
-            caches.open(CACHE_NAME)                                                 // ouvre le cache au nom de la variable placé en paramètre
-              .then(function(cache) {                                               // callback
-                cache.put(event.request, responseToCache);                          // place dans ce cache la requête (1er paramètre) et sa réponse (2e paramètre)
+            caches.open(CACHE_NAME)                                                     // ouvre le cache au nom de la variable placé en paramètre
+              .then(function(cache) {                                                   // callback
+                cache.put(event.request, responseToCache);                              // place dans ce cache la requête (1er paramètre) et sa réponse (2e paramètre)
               });
 
-            return response;                                                        // retourne la réponse
+            return response;                                                            // retourne la réponse
           }
         );
       })
@@ -64,24 +65,21 @@ self.addEventListener('fetch', function(event) {                                
 });
 
 
-
 /*--  UPDATE SERVICE WORKERS  -------------------------------------------------------*/
 
-
-self.addEventListener('activate', function(event) {                                 //  à l'évènement 'activate'
+self.addEventListener('activate', function(event) {                                     //  à l'évènement 'activate'
     console.log('activate cache');
-    
-    
-  //var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
-  var cacheWhitelist = ['my-site-cache-v2'];
 
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
+  //var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
+  var cacheWhitelist = ['my-site-cache-v2'];                                            // nouvelle cache
+
+  event.waitUntil(                                                                      // sur l'évènement, attendre jusqu'à (extend la période d'écoute de l'évènement pour permettre les opérations asynchrones)
+    caches.keys().then(function(cacheNames) {                                           // retourne une promesse avec le tableau des clés des caches
         console.log(cacheNames);
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
+      return Promise.all(                                                               // aggrégation des résultats, retourne une seule promesse 
+        cacheNames.map(function(cacheName) {                                            // boucle sur les résultats
+          if (cacheWhitelist.indexOf(cacheName) === -1) {                               // si le résultat est undefined                                
+            return caches.delete(cacheName);                                            // ce résultat est supprimer
           }
         })
       );
